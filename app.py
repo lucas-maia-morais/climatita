fda20fe84e799d6918492498db17341a00bffd92
lucas-maia-morais <lucasmaiamorais@gmail.com>
2022-07-03 17:32:11 -0300

    CRUD complete

--- app.py
+++ app.py
@@ -1,36 +1,54 @@
+from crypt import methods
+from email import message
 from operator import methodcaller
+from urllib import response
 import requests
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify, redirect, flash, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 import aux
 
+# Start app
 app = Flask(__name__)
 app.config['DEBUG'] = True
+
+# Connect db
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
 
 db = SQLAlchemy(app)
+
+# enable CORS
+CORS(app, resources={r'/*': {'origins': '*'}})
 
 class City(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
 
 @app.route('/', methods=['GET','POST'])
+@app.route('/index', methods=['GET','POST'])
 def index():
+    
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=f2356b58400a9681ea05ad7545fae07e&units=metric'
+
     if request.method == 'POST':
-        new_city = request.form.get('city')
-        print(new_city)
-        if new_city:
-            new_city_obj = City(name=new_city)
-            db.session.add(new_city_obj)
-            db.session.commit()
+        try:
+            new_city = request.form.get('city')
+            r = requests.get(url.format(new_city)).json()
+
+            exists = City.query.filter_by(name=r['name']).first()
+
+            if exists is None:
+                new_city_obj = City(name=r['name'])
+                db.session.add(new_city_obj)
+                db.session.commit()
+        except:
+            pass
 
     cities = City.query.all()
     cities = [city.name for city in cities]
     cities = list(dict.fromkeys(cities))
 
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=f2356b58400a9681ea05ad7545fae07e&units=metric'
-    
     weather_data = []
 
     for city in cities:
@@ -50,3 +68,129 @@ def index():
 
 
     return render_template('weather.html', weather_data = weather_data)
+
+@app.route('/<city_name>/delete_city', methods=['POST'])
+def delete_city(city_name):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=f2356b58400a9681ea05ad7545fae07e&units=metric'
+    try:
+        r = requests.get(url.format(city_name)).json()
+
+        cities = City.query.filter_by(name=r['name'])
+
+        for city in cities:
+            db.session.delete(city)
+        db.session.commit()
+
+        flash("City Deleted")
+    except:
+        pass
+
+    return redirect(url_for('index'))
+
+@app.route('/delete_all', methods=['POST'])
+def delete_all():
+    try:
+        cities = City.query.all()
+        for city in cities:
+            db.session.delete(city)
+
+        db.session.commit()
+    except:
+        pass
+    
+    return redirect(url_for('index'))
+
+
+@app.route('/cities_weather', methods=['GET','POST'])
+def all_cities_weather():
+    response_object = {'status': 'success'}
+
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=f2356b58400a9681ea05ad7545fae07e&units=metric'
+    
+    if request.method == 'POST':
+        try:
+            new_city = request.get_json()
+            r = requests.get(url.format(new_city['city'])).json()
+            exists = City.query.filter_by(name=r['name']).first()
+
+            if exists is None:
+                new_city_obj = City(name=r['name'])
+                db.session.add(new_city_obj)
+                db.session.commit()
+                response_object['message'] = 'Cidade Adicionada!'
+            else:
+                response_object['message'] = 'Cidade ja foi adicionada anteriormente!'
+        except:
+            response_object['message'] = 'Erro na adicao! Tente mais tarde.'
+            response_object['status'] = 'failed'
+    else:
+        weather_data = []
+        cities = City.query.all()
+        # print(cities)
+        # cities = [city.name for city in cities]
+        # print(cities)
+        # cities = list(dict.fromkeys(cities))
+        # print(cities)
+
+        for city in cities:
+            try:
+                name = city.name
+                r = requests.get(url.format(name)).json()
+                weather = {
+                    'city_id': city.id,
+                    'city': r['name'],
+                    'temperature': int(float(r['main']['temp'])),
+                    'description': r['weather'][0]['description'],
+                    'dica': aux.dica(r['main']['temp']),
+                    'icon': r['weather'][0]['icon'],
+                }
+                weather_data.append(weather)
+            except:
+                pass
+        
+        response_object['cities_weather'] = weather_data
+
+    return jsonify(response_object)
+
+@app.route('/cities_weather/<city_id>', methods=['PUT','DELETE'])
+def single_city(city_id):
+    
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=f2356b58400a9681ea05ad7545fae07e&units=metric'
+    response_object = {'status': 'success'}
+    old_city_obj = City.query.filter_by(id=city_id).first()
+
+    if request.method == 'PUT':
+        post_data = request.get_json()
+        
+        update_city = post_data.get('city')
+        r = requests.get(url.format(update_city)).json()
+
+        if old_city_obj.name == r['name']:
+            response_object['message'] = 'Cidade é a mesma!'
+        else:
+            try:
+                new_city_obj = City(name=r['name'])
+                db.session.delete(old_city_obj)
+                db.session.add(new_city_obj)
+                db.session.commit()
+                response_object['message'] = 'Cidade Atualizada!'
+            except:
+                response_object['message'] = 'Erro na atualização! Tente mais tarde'
+
+    if request.method == 'DELETE':        
+        try:
+            db.session.delete(old_city_obj)
+            db.session.commit()
+            response_object['message'] = 'Cidade Deletada!'
+        except:
+            response_object['message'] = 'Erro na deleção! Tente mais tarde'
+
+    return jsonify(response_object)
+
+# sanity check route
+@app.route('/ping', methods=['GET'])
+def ping_pong():
+    return jsonify('pong!')
+
+if __name__ == '__main__':
+    app.run()
--- client/src/components/CitiesWeather.vue
+++ client/src/components/CitiesWeather.vue
@@ -4,7 +4,9 @@
       <div class="col-sm-10">
         <h1>Climatita das cidades</h1>
         <hr><br><br>
-        <button type="button" class="btn btn-success btn-sm">Adicionar cidade</button>
+        <alert :message=message v-if="showMessage" show dismissible></alert>
+        <button type="button" class="btn btn-success btn-sm"
+          v-b-modal.cityWeather-modal>Adicionar cidade</button>
         <br><br>
         <table class="table table-hover">
           <thead>
@@ -28,8 +30,17 @@
               <td><b>Dica do dia: </b>{{ weather.dica }}</td>
               <td>
                 <div class="btn-group" role="group">
-                  <button type="button" class="btn btn-warning btn-sm">Update</button>
-                  <button type="button" class="btn btn-danger btn-sm">Delete</button>
+                  <button
+                          type="button"
+                          class="btn btn-warning btn-sm"
+                          v-b-modal.cityWeather-update-modal
+                          @click="editCityWeather(weather)">Update</button>
+                  <button
+                          type="button"
+                          class="btn btn-danger btn-sm"
+                          @click="onDeleteCityWeather(weather)">
+                      Delete
+                  </button>
                 </div>
               </td>
             </tr>
@@ -37,17 +48,70 @@
         </table>
       </div>
     </div>
+    <b-modal ref="addCityWeatherModal"
+            id="cityWeather-modal"
+            title="Adicione uma nova cidade"
+            hide-footer>
+      <b-form @submit="onSubmit" @reset="onReset" class="w-100">
+      <b-form-group id="form-city-group"
+                    label="Cidade:"
+                    label-for="form-city-input">
+          <b-form-input id="form-city-input"
+                        type="text"
+                        v-model="addCityWeatherForm.city"
+                        required
+                        placeholder="Cidade">
+          </b-form-input>
+        </b-form-group>
+        <b-button type="submit" variant="primary">Submit</b-button>
+        <b-button type="reset" variant="danger">Reset</b-button>
+      </b-form>
+    </b-modal>
+    <b-modal ref="editCityWeatherModal"
+            id="cityWeather-update-modal"
+            title="Update"
+            hide-footer>
+      <b-form @submit="onSubmitUpdate" @reset="onResetUpdate" class="w-100">
+      <b-form-group id="form-city-edit-group"
+                    label="Cidade:"
+                    label-for="form-city-edit-input">
+          <b-form-input id="form-city-edit-input"
+                        type="text"
+                        v-model="editForm.city"
+                        required
+                        placeholder="Cidade">
+          </b-form-input>
+        </b-form-group>
+        <b-button-group>
+          <b-button type="submit" variant="primary">Update</b-button>
+          <b-button type="reset" variant="danger">Cancel</b-button>
+        </b-button-group>
+      </b-form>
+    </b-modal>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+import Alert from './Alert.vue';
 
 export default {
   data() {
     return {
       weather_data: [],
+      addCityWeatherForm: {
+        city: '',
+      },
+      message: '',
+      showMessage: false,
+      editForm: {
+        city_id: '',
+        city: '',
+      },
     };
+  },
+  components: {
+    alert: Alert,
   },
   methods: {
     getWeatherData() {
@@ -60,6 +124,87 @@ export default {
           // eslint-disable-next-line
           console.error(error);
         });
+    },
+    addCityWeather(payload) {
+      const path = 'http://localhost:5000/cities_weather';
+      axios.post(path, payload)
+        .then((res) => {
+          this.getWeatherData();
+          this.message = res.data.message;
+          this.showMessage = true;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.log(error);
+          this.getWeatherData();
+        });
+    },
+    initForm() {
+      this.addCityWeatherForm.city = '';
+      this.editForm.city_id = '';
+      this.editForm.city = '';
+    },
+    onSubmit(evt) {
+      evt.preventDefault();
+      this.$refs.addCityWeatherModal.hide();
+      const payload = {
+        city: this.addCityWeatherForm.city,
+      };
+      this.addCityWeather(payload);
+      this.initForm();
+    },
+    onReset(evt) {
+      evt.preventDefault();
+      this.$refs.addCityWeatherModal.hide();
+      this.initForm();
+    },
+    editCityWeather(weather) {
+      this.editForm.city_id = weather.city_id;
+      this.editForm.city = weather.city;
+    },
+    onSubmitUpdate(evt) {
+      evt.preventDefault();
+      this.$refs.editCityWeatherModal.hide();
+      const payload = {
+        city: this.editForm.city,
+      };
+      this.updateCityWeather(payload, this.editForm.city_id);
+    },
+    updateCityWeather(payload, cityID) {
+      const path = `http://localhost:5000/cities_weather/${cityID}`;
+      axios.put(path, payload)
+        .then((res) => {
+          this.getWeatherData();
+          this.message = res.data.message;
+          this.showMessage = true;
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+          this.getWeatherData();
+        });
+    },
+    onResetUpdate(evt) {
+      evt.preventDefault();
+      this.$refs.editCityWeatherModal.hide();
+      this.initForm();
+      this.getWeatherData();
+    },
+    removeCityWeather(cityID) {
+      const path = `http://localhost:5000/cities_weather/${cityID}`;
+      axios.delete(path)
+        .then((res) => {
+          this.getWeatherData();
+          this.message = res.data.message;
+          this.showMessage = true;
+        })
+        .catch((error) => {
+          console.error(error);
+          this.getWeatherData();
+        });
+    },
+    onDeleteCityWeather(weather) {
+      this.removeCityWeather(weather.city_id);
     },
   },
   created() {
